0af161edc9f2a3420f8eb6c7a58fcacf847c5f79
mhcuevas <mike.henderson@ing.uchile.cl>
2020-12-03 10:00:02 -0300

    Update BLElink_v1.ino
    Ultima versión enlace BLE a hardware en bicicleta (Bike Tracker)

--- BLElink_v1/BLElink_v1.ino
+++ BLElink_v1/BLElink_v1.ino
@@ -7,28 +7,59 @@ SoftwareSerial BTserial(10, 9); // RX | TX
 //---------------------
 const String DEF = "DEFAULT";
 String msg_rcv;
-int suggestion = 0;
+int suggestion = -1;
 char c;
+
+//Motor A
+int STBY = 6; //standby
+int PWMA = 3; //Speed control
+int AIN1 = 7; //Direction
+int AIN2 = 8; //Direction
 //---------------------
 // Functions
 //---------------------
+//Motor functions
+void move(int speed, int direction){
+//Move specific motor at speed and direction
+//motor: 0 for B 1 for A
+//speed: 0 is off, and 255 is full speed
+//direction: 0 clockwise, 1 counter-clockwise
+digitalWrite(STBY, HIGH); //disable standby
+
+boolean inPin1 = LOW;
+boolean inPin2 = HIGH;
+
+if(direction == 1){
+inPin1 = HIGH;
+inPin2 = LOW;
+}
+
+digitalWrite(AIN1, inPin1);
+digitalWrite(AIN2, inPin2);
+analogWrite(PWMA, speed);
+
+}
+//Stop motor
+void stop(){
+digitalWrite(STBY, LOW);
+}
 // Sets suggested acc. (setSuggestion) and retrieve relevant data (retrieveData)
 void dataAndSuggestionExchange(String msg) {
   if (msg != DEF) { 
   setSuggestion(msg);
   }
-  retrieveData();
+  //retrieveData();
   Serial.println(msg);
   msg_rcv = DEF;
   }
 // Sets suggested acc.
 void setSuggestion(String msg) {
   if (msg == "+") {suggestion = 1;}
-  if (msg == "0") {suggestion = 0;}
-  if (msg == "-") {suggestion = -1;}
+  if (msg == "0") {suggestion = -1;}
+  if (msg == "-") {suggestion = 0;}
   }
 // Retrieves formated data throug BLE link.
-void retrieveData(){
+/*void retrieveData(){
   String msg2Send = "{V:" + getSpeedString()+",A:" + getSuggestion() + "}";
   
   char __dataFile[msg2Send.length() + 1];
@@ -59,8 +90,18 @@ String getSuggestion(){
 float getSpeed() {
   float spd = 1.463;
   return spd;
-  }
-  
+  }*/
+
+void suggestionUpdate(int suggestion) {
+   if (suggestion != -1) {
+    move(150, suggestion);
+   }
+   else {
+    stop();
+    delay(250);
+   }
+   Serial.println(suggestion);
+}
 //---------------------
 // Setup & Loop
 //---------------------
@@ -72,6 +113,13 @@ void setup()
  Serial.println("Remember to select Both NL & CR in the serial monitor");
  // HC-05 default serial speed for AT mode is 38400
  BTserial.begin(9600);
+
+ //Motor related
+ pinMode(STBY, OUTPUT);
+
+ pinMode(PWMA, OUTPUT);
+ pinMode(AIN1, OUTPUT);
+ pinMode(AIN2, OUTPUT);
 }
 
 void loop()
@@ -85,5 +133,6 @@ void loop()
     }
  }
  dataAndSuggestionExchange(msg_rcv);
+ suggestionUpdate(suggestion);
  delay(1000);
 }
